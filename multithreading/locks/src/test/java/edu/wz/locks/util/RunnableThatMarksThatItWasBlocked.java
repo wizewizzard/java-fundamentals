9759c2fe57b8e5eeb3aaa82cb83badc02fb7532b
wizewizzard <kingaal93@gmail.com>
2022-07-03 21:43:10 +0700

    Add reentr. locks and r/w locks tests

---
+++ multithreading/locks/src/test/java/edu/wz/locks/util/RunnableThatMarksThatItWasBlocked.java
@@ -0,0 +1,44 @@
+package edu.wz.locks.util;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+
+public class RunnableThatMarksThatItWasBlocked implements Runnable{
+    private boolean wasBlocked = false;
+
+    private final ReadWriteLock readWriteLock;
+
+    public boolean wasItBlocked() {
+        return wasBlocked;
+    }
+
+    public RunnableThatMarksThatItWasBlocked(ReadWriteLock readWriteLock) {
+        this.readWriteLock = readWriteLock;
+    }
+
+    @Override
+    public void run() {
+        Lock readLock = readWriteLock.readLock();
+        for(int i = 0; i < 10; i ++){
+            try{
+                Thread.sleep(100);
+                boolean lockAcquired = readLock.tryLock();
+                if(lockAcquired){
+                    try {
+                        //blah blah reading...
+                    }
+                    finally {
+                        readLock.unlock();
+                    }
+                }
+                else{
+                    this.wasBlocked = true;
+                }
+            }
+            catch (InterruptedException ignored){
+
+            }
+        }
+
+    }
+}
